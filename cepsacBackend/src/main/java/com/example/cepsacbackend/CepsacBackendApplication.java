59b54729ae395e79557046b0944c573ba14459c8
ElderJV <elderjulcavelasco@gmail.com>
2025-09-22 09:42:02 -0500

    Base Proyecto

---
+++ cepsacBackend/src/main/java/com/example/cepsacbackend/CepsacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cepsacbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepsacBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CepsacBackendApplication.class, args);
+    }
+
+}
---
+++ cepsacBackend/src/test/java/com/example/cepsacbackend/CepsacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cepsacbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepsacBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
